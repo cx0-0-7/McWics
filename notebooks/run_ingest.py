7b94696e5ec0c872ade8d1f4f114e7866ea037ef
celina x. <celinaxu2006@gmail.com>
2026-02-08 10:01:35 -0500

    more fix!

--- notebooks/run_ingest.py
+++ notebooks/run_ingest.py
@@ -1,13 +1,19 @@
 # Databricks notebook source
+
+%pip install python-jobspy pymongo[srv] google-generativeai
+
+# COMMAND ----------
+
 import sys
 import os
 
-
-# --- 1. ENVIRONMENT SETUP ---
 project_root = os.path.abspath('..')
 if project_root not in sys.path:
     sys.path.append(project_root)
 
+print(f"Path configured: {project_root}")
+
+# COMMAND ----------
 from src.transformations.silver_clean import silver_processing
 
 final_silver_df = silver_processing()
@@ -15,6 +21,4 @@ final_silver_df = silver_processing()
 if final_silver_df is not None:
     # We use display() to see the beautiful, cleaned interactive table
     display(final_silver_df)
-else:
-    print("Silver processing returned no data..")
 
